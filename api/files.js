168780384c4463e974396e917018cfb537b4f81b
Tuan Pham <ttpham0111@gmail.com>
2018-08-06 13:07:18 -0700

    heroku test

--- api/files.js
+++ api/files.js
@@ -6,29 +6,13 @@ const express = require('express')
 const mkdirp = require('mkdirp')
 const path = require('path')
 const uuid = require('uuid/v4')
-const fs = require('fs')
 
 const router = express.Router()
 
 const staticDir = path.join(__dirname, '..', 'static', 'experiences')
 const staticUrl = '/static/experiences'
 
-function ls(dirname) {
-  fs.readdir(dirname, (err, files) => {
-    if (err) console.log(err)
-    else {
-      files.forEach(file => {
-        console.log(dirname, file)
-      })
-    }
-  })
-}
-
 router.post('/', (req, res) => {
-  ls(staticDir)
-  ls(path.join(staticDir, 'img'))
-  ls('./static')
-
   // TODO: Move to middleware, check exists, error handle
   admin.auth().verifyIdToken(req.header('Authorization').split(' ')[1])
     .then(userData => {
@@ -37,9 +21,10 @@ router.post('/', (req, res) => {
 
       const destDir = path.join(staticDir, 'img', userData.uid)
       const dest = path.join(destDir, filename)
-      
 
-      mkdirp.sync(destDir)
+      mkdirp.sync(destDir, {
+        mode: '0755'
+      })
       return req.files.image.mv(dest)
         .then(() => {
           // TODO: Won't display outside of server, use non-local storage
