53a206454e3d75c65a0ccb2ceca5e18f498b4676
Tuan Pham <ttpham0111@gmail.com>
2018-08-06 11:29:49 -0700

    Add prod.env for heroku

---
+++ config/prod.env.js
@@ -0,0 +1,7 @@
+// TODO: This is not included in .gitignore 
+// since it's required in build.js and heroku
+// auto deploys from github.
+
+// Add to .gitignore before updating
+module.exports = {
+}
